0421eb748e3829f4dcb354fbeebe6dcbf5a73da2
Rad <chihaby@gmail.com>
2026-07-24 18:45:28 -0700

    ready for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
   return (
     <main
       style={{
-        minHeight: "100vh",
+        minHeight: "90vh",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
